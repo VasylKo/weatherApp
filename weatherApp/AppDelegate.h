14b38eb780cb2ae861d154aca9d19135f0c92c4c
Vasyl Kotsiuba <clic@ukr.net>
2015-11-08 22:30:09 +0200

    Initial Commit

---
+++ weatherApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  weatherApp
+//
+//  Created by Vasyl Kotsiuba on 11/8/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ weatherApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  weatherApp
+//
+//  Created by Vasyl Kotsiuba on 11/8/15.
+//  Copyright © 2015 Vasyl Koysiuba. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
